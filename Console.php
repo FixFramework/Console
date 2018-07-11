201d3b31e8ad8076c64032863eda8ed21708a4a1
Cengiz AKCAN <cengiz@zekicetasarim.com>
2018-07-12 02:58:27 +0300

    Add files via upload

---
+++ Console.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace console;
+
+
+
+class Console
+{
+
+
+    /**
+     * Console constructor.
+     * @param $Map
+     * @param $Request
+     */
+    public function __construct($Map, $Request){
+
+
+
+    }
+
+
+}
